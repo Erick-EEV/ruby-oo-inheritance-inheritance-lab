824f1ab8f10ce6f4f7aa7d951cfb4cd6d502ddbd
Erick-EEV <EEVERICKEEV@gmail.com>
2020-10-14 09:11:51 -0400

    Beginning Inheiritence

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+class Student < User
 
+    def initialize
+        @knowledge = []
+        super
+    end
+
+    def new
+        @knowledge
+    end
+
+    def learn(random_string)
+        @knowledge << random_string
+    end
+
+    def knowledge
+        @knowledge
+    end
 end
